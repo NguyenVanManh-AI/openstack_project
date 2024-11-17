134fb02899cb4e963fad65263e6a7127237af2fd
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-11-17 14:58:09 +0700

    Similar to the ubuntu64bit vmware virtual machine, access the ubuntu 16 machine, use the ip a command to see the network interface eth0 or ens33,...

--- vrrp_setup.sh
+++ vrrp_setup.sh
@@ -1,55 +1,42 @@
 #!/bin/bash
 
-# Xác định tên máy chủ (hostname)
 HOSTNAME=$(hostname)
 
-# Thiết lập trạng thái và ưu tiên của Keepalived
 if [ "$HOSTNAME" == "vrrp-master" ]; then
     KEEPALIVED_STATE='MASTER'
     KEEPALIVED_PRIORITY=100
-elif [ "$HOSTNAME" == "vrrp-slave" ]; then
+elif [ "$HOSTNAME" == "vrrp-backup" ]; then
     KEEPALIVED_STATE='BACKUP'
     KEEPALIVED_PRIORITY=50
 else
-    echo "Invalid hostname $HOSTNAME for the setup script $0"
-    exit 1
+    echo "invalid hostname $HOSTNAME for install script $0";
+    exit 1;
 fi
 
-# Lấy địa chỉ IP của giao diện eth0
-IP=$(ip addr | grep inet | grep eth0 | awk '{print $2}' | cut -d'/' -f1)
+IP=$(ip addr | grep inet | grep ens3 | awk '{print $2}' | cut -d'/' -f1)
 
-# Thêm địa chỉ IP và hostname vào /etc/hosts
 echo "$IP $HOSTNAME" >> /etc/hosts
 
-# Cập nhật và cài đặt các gói cần thiết
 apt-get update
-apt-get -y install keepalived apache2
+apt-get -y install keepalived
 
-# Cấu hình Keepalived
-cat <<EOL > /etc/keepalived/keepalived.conf
-vrrp_instance vrrp_group_1 {
+echo "vrrp_instance vrrp_group_1 {
     state $KEEPALIVED_STATE
-    interface eth0
+    interface ens3
     virtual_router_id 1
     priority $KEEPALIVED_PRIORITY
-    advert_int 1
     authentication {
         auth_type PASS
         auth_pass password
     }
     virtual_ipaddress {
-        10.10.10.200/24
+        10.10.10.200/24 brd 10.10.10.255 dev ens3
     }
-}
-EOL
+}" > /etc/keepalived/keepalived.conf
 
-# Tạo trang web đơn giản để xác minh máy chủ đang hoạt động
+apt-get -y install apache2
 echo "$HOSTNAME" > /var/www/html/index.html
-
-# Khởi động lại dịch vụ Keepalived
 service keepalived restart
 
-# Kiểm tra trạng thái dịch vụ Keepalived
-systemctl status keepalived
-
-echo "Keepalived setup completed for $HOSTNAME"
+echo "Configuring NAT"
+sudo iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE
